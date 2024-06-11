5fb15101c84037ecf2103763d637492d1fe1f780
Tenarien <mamateki7@gmail.com>
2024-06-12 00:19:33 +0100

    Create boilerplate files, for tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+
+var toggleOpen = document.getElementById('toggleOpen');
+var toggleClose = document.getElementById('toggleClose');
+var collapseMenu = document.getElementById('collapseMenu');
+
+function handleClick() {
+    if (collapseMenu.style.display === 'block') {
+        collapseMenu.style.display = 'none';
+    } else {
+        collapseMenu.style.display = 'block';
+    }
+}
+
+toggleOpen.addEventListener('click', handleClick);
+toggleClose.addEventListener('click', handleClick);
