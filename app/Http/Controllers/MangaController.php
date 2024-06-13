18da12e49b68a7743dec3c122a677ea8618f59d0
Tenarien <mamateki7@gmail.com>
2024-06-13 22:18:17 +0100

    Created method to store new mangas (not needed for now), and methods to show chapters, follow and unfollow mangas

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Manga;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class MangaController extends Controller
 {
@@ -12,5 +14,43 @@ class MangaController extends Controller
         $mangas = Manga::latest()->paginate(10);
 
         return view('mangas.index', ['mangas' => $mangas]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'max:255'],
+            'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        ]);
+
+        $path = null;
+        if ($request->hasFile('image')) {
+            $path = Storage::disk('public')->put('images', $request->image);
+        }
+
+        Manga::create([
+            'title' => $request->input('title'),
+            'image' => $path,
+        ]);
+
+        return redirect()->route('mangas.index');
+    }
+
+    public function show(Manga $manga)
+    {
+        $manga->load('chapters');
+
+        return view('mangas.show', ['manga' => $manga]);
+    }
+
+    public function follow(Manga $manga)
+    {
+        $manga->follow(Auth::user());
+        return redirect()->back()->with('success', 'You are now following this manga.');
+    }
+
+    public function unfollow(Manga $manga)
+    {
+        $manga->unfollow(Auth::user());
+        return redirect()->back()->with('success', 'You are no longer following this manga.');
     }
 }
