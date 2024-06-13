03b777226abea60dce1be882bf9b5f14aa81deb2
Tenarien <mamateki7@gmail.com>
2024-06-13 22:13:46 +0100

    Small design correction

--- resources/views/mangas/index.blade.php
+++ resources/views/mangas/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
 
     <h1 class="title">Latest Mangas</h1>
-    <div class="flex justify-between">
-        <div class="grid gap-4 card">
+    <div class="flex justify-center">
+        <div class="grid grid-cols-2 gap-4 card">
             @foreach ($mangas as $manga)
                 <x-mangaCard :manga="$manga" />
             @endforeach
