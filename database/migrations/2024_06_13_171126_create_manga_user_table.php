2eabd48b195fda3434eb40365105c5e7c70b5dcb
Tenarien <mamateki7@gmail.com>
2024-06-13 22:12:24 +0100

    Create a new table that can link a user to a manga, add a new column to store url to that manga

--- database/migrations/2024_06_13_171126_create_manga_user_table.php
+++ database/migrations/2024_06_13_171126_create_manga_user_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mangas', function (Blueprint $table) {
+        Schema::create('manga_user', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('image')->nullable();
-            $table->text('url');
+            $table->foreignId('manga_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mangas');
+        Schema::dropIfExists('manga_user');
     }
 };
