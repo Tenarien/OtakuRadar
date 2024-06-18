30da38be42d4340bca9865d890437bfa32c29240
Tenarien <mamateki7@gmail.com>
2024-06-18 17:19:31 +0100

    Added methods to help store data if user viewed a chapter, and to determine next and previous chapter

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -20,8 +20,52 @@ class Chapter extends Model
         return $this->belongsTo(Manga::class);
     }
 
+    public function views()
+    {
+        return $this->hasMany(ChapterView::class);
+    }
+
     public function links(): HasMany
     {
         return $this->hasMany(Link::class);
+    }
+
+    public function nextChapter()
+    {
+        return $this->manga->chapters()
+            ->where('id', '>', $this->id)
+            ->orderBy('id', 'asc')
+            ->first();
+    }
+
+    public function previousChapter()
+    {
+        return $this->manga->chapters()
+            ->where('id', '<', $this->id)
+            ->orderBy('id', 'desc')
+            ->first();
+    }
+
+    public function nextLinkUrl()
+    {
+        $nextChapter = $this->nextChapter();
+        return $nextChapter ? $nextChapter->links->first()?->url : null;
+    }
+
+    public function previousLinkUrl()
+    {
+        $previousChapter = $this->previousChapter();
+        return $previousChapter ? $previousChapter->links->first()?->url : null;
+    }
+
+    public function chapterViews()
+    {
+        return $this->hasMany(ChapterView::class);
+    }
+
+
+    public function wasViewedByUser($userId)
+    {
+        return $this->chapterViews()->where('user_id', $userId)->exists();
     }
 }
