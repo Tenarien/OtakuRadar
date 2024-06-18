dec45e5f8eb56698a2858ad491c38c87e92fad6c
Tenarien <mamateki7@gmail.com>
2024-06-18 17:14:48 +0100

    Added a new table to store information if user read a chapter

---
+++ database/migrations/2024_06_17_195532_create_chapter_views_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapter_views', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('chapter_id')->constrained()->onDelete('cascade');
+            $table->boolean('viewed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapter_views');
+    }
+};
