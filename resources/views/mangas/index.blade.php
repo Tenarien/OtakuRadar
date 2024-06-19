1003abbd9c0adaf9e1f7ded89ff3ca4e3dee30b1
Tenarien <mamateki7@gmail.com>
2024-06-19 20:32:49 +0100

    Modify classes to allow change of colors when dark mode is applied

--- resources/views/mangas/index.blade.php
+++ resources/views/mangas/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
 
-    <h1 class="title">Latest Mangas</h1>
-    <div class="card flex justify-center">
-        <div class="grid sm:grid-cols-1 md:grid-cols-2 md:gap-6 xl:gap-12">
+    <h1 class="title dark:text-slate-200">Latest Mangas</h1>
+    <div class="card flex justify-center dark:bg-gray-600">
+        <div class="grid sm:grid-cols-1 md:grid-cols-2 md:gap-6 xl:gap-12 dark:bg-gray-600">
             @foreach ($mangas as $manga)
                 <x-mangaCard :manga="$manga"/>
             @endforeach
