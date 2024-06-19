0e9e320de92bb292cd07bfc43f33ff4f32e19bac
Tenarien <mamateki7@gmail.com>
2024-06-19 20:34:01 +0100

    Add a property darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
+        "./resources/js/**/*.js",
+        "./resources/css/**/*.css",
         "./resources/**/*.vue",
     ],
+    darkMode: 'class',
   theme: {
     extend: {},
   },
